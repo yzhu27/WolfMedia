dd0ac6a16de5cdaed0085df9a8726e62e04c8378
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-09 19:53:11 -0400

    Create enterArtistInfo.java

---
+++ src/Information Processing/enterArtistInfo.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class enterArtistInfo {
+
+	public static ExecResult run(Scanner reader) {
+		System.out.println("+------------------------------------+");
+		System.out.println("| Please Submit the Following Inputs |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+		System.out.println("Artist ID: ");
+		int artistID = reader.nextInt();
+		reader.nextLine();
+
+		System.out.println("Name: ");
+		String name = reader.nextLine();
+
+		System.out.println("Status: ");
+		String status = reader.nextLine();
+
+		System.out.println("Type: ");
+		String type = reader.nextLine();
+
+		System.out.println("Art Country: ");
+		String artCountry = reader.nextLine();
+
+		System.out.println("Monthly Listeners: ");
+		int monthlyListeners = reader.nextInt();
+		reader.nextLine();
+
+		System.out.println("Primary Genre: ");
+		String primaryGenre = reader.nextLine();
+
+		System.out.println("Record Label ID: ");
+		int rlID = reader.nextInt();
+		reader.nextLine();
+
+		return execute(artistID, name, status, type, artCountry, monthlyListeners, primaryGenre, rlID);	
+	}
+
+	public static ExecResult execute(int artistID, String name, String status, String type, String artCountry, int monthlyListeners, String primaryGenre, int rlID) {
+		
+		String sql = 
+			"INSERT INTO Artists VALUES "  + "\n" + "\t" +
+				"(%d, '%s', '%s', '%s', '%s', %d, '%s', %d)"  + "\n" +
+			";" + "\n" + "\n"
+		;
+		
+		sql = String.format(sql, artistID, name, status, type, artCountry, monthlyListeners, primaryGenre, rlID);
+		
+		return WolfPubDB.executeUpdate(sql);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("\n");
+		System.out.println("Unit Test for AddArtist");
+		System.out.println("===============================");
+		execute(1, "Ed Sheeran", "Active", "Solo", "United Kingdom", 150000000, "Pop", 1);
+	}
+
+}
