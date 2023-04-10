5df7f71d71dfc2096bb1811b22d66ed38664a31e
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-10 17:12:22 -0400

    Update enterEpisodeInfo.java

--- src/Information Processing/enterEpisodeInfo.java
+++ src/Information Processing/enterEpisodeInfo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class enterEpisodeInfo {
     
-    public static ExecResult run(Scanner reader) {
+    public static Result run(Scanner reader) {
         System.out.println("+------------------------------------+");
         System.out.println("| Please Submit the Following Inputs |");
         System.out.println("+------------------------------------+");
@@ -37,18 +37,15 @@ public class enterEpisodeInfo {
         return execute(peID, pID, peTitle, peDuration, peReleaseDate, listenerCount, adCount);
     }
 
-    public static ExecResult execute(int peID, int pID, String peTitle, String peDuration, String peReleaseDate, int listenerCount, int adCount) {
+    public static Result execute(int peID, int pID, String peTitle, String peDuration, String peReleaseDate, int listenerCount, int adCount) {
         String sql = "INSERT INTO PodcastEpisodes (PEID, PID, PETitle, PEDuration, PEReleaseDate, ListenerCount, AdCount) VALUES (%d, %d, '%s', '%s', '%s', %d, %d);";
 
         sql = String.format(sql, peID, pID, peTitle, peDuration, peReleaseDate, listenerCount, adCount);
 
-        return WolfPubDB.executeUpdate(sql);
+        return Connect.executeUpdate(sql);
     }
 
     public static void main(String[] args) {
-        System.out.println("\n");
-        System.out.println("Unit Test for AddPodcastEpisode");
-        System.out.println("===============================");
-        execute(1, 1, "Episode 1", "00:25:00", "2023-04-09", 500, 3);
+        
     }
 }
