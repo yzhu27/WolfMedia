d3ee8357c49983eead9518c8e0e222d8a6899439
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-09 20:38:59 -0400

    Create deleteSongInfo.java

---
+++ src/Information Processing/deleteSongInfo.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class deleteSongInfo {
+
+    public static void showDetails(String tableName){
+        String sql = String.format("SELECT * FROM " + tableName + ";");
+        System.out.println("sql::" + sql);
+		WolfPubDB.executeQuery(sql);
+    }
+
+    public static ExecResult execute(int songID) {
+
+        ExecResult result = null;
+
+        String sql = 
+			"DELETE FROM Songs WHERE SongID = %d"  + "\n" + "\t" +
+				 "\n" +
+			";" + "\n" + "\n"
+		;
+        
+		sql = String.format(sql, songID);
+
+		return WolfPubDB.executeUpdate(sql);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("\n");
+		System.out.println("Unit Test for DeleteBookEdition");
+		System.out.println("===============================");
+		execute(1);
+	}
+
+
+
+    public static ExecResult run(Scanner reader) {
+
+		System.out.println("+------------------------------------+");
+		System.out.println("|         Book Details               |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+        showDetails("Songs");
+
+		System.out.println("+------------------------------------+");
+		System.out.println("| Please Submit the Following Inputs |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+		System.out.println("Song ID: ");
+		int songID = reader.nextInt();
+		reader.nextLine();
+
+		return execute(songID);	
+	}
+
+}
