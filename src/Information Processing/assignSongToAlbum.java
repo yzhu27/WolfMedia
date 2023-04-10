80ff2f614038f07865fabc934becac74c845ce06
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-10 16:03:22 -0400

    Create assignSongToAlbum.java

---
+++ src/Information Processing/assignSongToAlbum.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class assignSongToAlbum {
+
+    public static Result execute(String sql) {
+        return Connect.executeUpdate(sql);
+    }
+
+    public static void showDetails(String tableName){
+        String sql = String.format("SELECT * FROM " + tableName + ";");
+        Connect.executeQuery(sql);
+    }
+
+    public static ExecResult run(Scanner reader) {
+        System.out.println("+------------------------------------+");
+        System.out.println("|           Song Details            |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        showDetails("Songs");
+
+        System.out.println("+------------------------------------+");
+        System.out.println("| Please Submit the Following Inputs |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        System.out.println("Song ID: ");
+        int songID = reader.nextInt();
+        reader.nextLine();
+
+         System.out.println("Album ID: ");
+        int albumID = reader.nextInt();
+        reader.nextLine();
+
+        String sql = "";
+        sql = "UPDATE Songs SET AlbumID=%d WHERE SongID = (%d);" + "\n" + "\n";
+        sql = String.format(sql, albumID, songID);
+        }
+
+        return execute(sql);
+    }
+}
