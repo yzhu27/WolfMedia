e4754611098bbd7aecf6a231bdba439b3fe3be0d
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-10 10:05:23 -0400

    Update updateSongInfo.java

--- src/Information Processing/updateSongInfo.java
+++ src/Information Processing/updateSongInfo.java
@@ -1,72 +1,62 @@
 import java.util.Scanner;
 import java.sql.*;
 
-public class UpdateArticle {
+public class updateSongInfo {
 
     public static ExecResult execute(String sql) {
-        
-		return WolfPubDB.executeUpdate(sql);
-	}
-
-   public static void showDetails(String tableName){
-        String sql = String.format("SELECT * FROM " + tableName + ";");
-		WolfPubDB.executeQuery(sql);
+        return WolfPubDB.executeUpdate(sql);
     }
 
+    public static void showDetails(String tableName){
+        String sql = String.format("SELECT * FROM " + tableName + ";");
+        WolfPubDB.executeQuery(sql);
+    }
 
     public static ExecResult run(Scanner reader) {
+        System.out.println("+------------------------------------+");
+        System.out.println("|           Song Details            |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        showDetails("Songs");
 
         System.out.println("+------------------------------------+");
-		System.out.println("|         Article Details         |");
-		System.out.println("+------------------------------------+");
-		System.out.println("");
+        System.out.println("| Please Submit the Following Inputs |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
 
-        showDetails("Article");
-
-		System.out.println("+------------------------------------+");
-		System.out.println("| Please Submit the Following Inputs |");
-		System.out.println("+------------------------------------+");
-		System.out.println("");
-
-		System.out.println("Publication ID: ");
-		int publicationID = reader.nextInt();
-		reader.nextLine();
+        System.out.println("Song ID: ");
+        int songID = reader.nextInt();
+        reader.nextLine();
 
         System.out.println("Attribute you want to update: ");
-		String attributeName = reader.nextLine();
+        String attributeName = reader.nextLine();
 
         System.out.println("New attribute value: ");
 
         String sql = "";
 
-        if(attributeName.equals("Title") || attributeName.equals("CreationDate")){
-            
-		    String updatedAttributeValue = reader.nextLine();
+        if(attributeName.equals("Title") || attributeName.equals("ReleaseCountry") || attributeName.equals("Slanguage")){
+            String updatedAttributeValue = reader.nextLine();
 
-            sql = 
-			"UPDATE Article SET %s='%s' WHERE PublicationID = "  + "\n" + "\t" +
-				"(%d)"  + "\n" +
-			";" + "\n" + "\n"
-		    ;
-        
-		    sql = String.format(sql, attributeName,updatedAttributeValue,publicationID);
+            sql = "UPDATE Songs SET %s='%s' WHERE SongID = (%d);" + "\n" + "\n";
+            sql = String.format(sql, attributeName, updatedAttributeValue, songID);
 
-        } else if(attributeName.equals("SequenceNumber")){
+        } else if(attributeName.equals("Duration") || attributeName.equals("RoyaltyRate") || attributeName.equals("RoyaltyPaid")){
+            float updatedAttributeValue = reader.nextFloat();
+            reader.nextLine();
 
+            sql = "UPDATE Songs SET %s=%f WHERE SongID = (%d);" + "\n" + "\n";
+            sql = String.format(sql, attributeName, updatedAttributeValue, songID);
+
+        } else if(attributeName.equals("Playcount") || attributeName.equals("ArtistID") || attributeName.equals("AlbumID") || attributeName.equals("TrackNumber")){
             int updatedAttributeValue = reader.nextInt();
             reader.nextLine();
 
-            sql = 
-			"UPDATE Article SET %s=%d WHERE PublicationID = "  + "\n" + "\t" +
-				"(%d)"  + "\n" +
-			";" + "\n" + "\n"
-		    ;
-        
-		    sql = String.format(sql, attributeName,updatedAttributeValue,publicationID);
-
-        }	
+            sql = "UPDATE Songs SET %s=%d WHERE SongID = (%d);" + "\n" + "\n";
+            sql = String.format(sql, attributeName, updatedAttributeValue, songID);
+        }
 
         return execute(sql);
-	}
-
+    }
 }
