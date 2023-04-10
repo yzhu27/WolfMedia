f28938a4635f0433fdf97ccac8665b1af0bdfdf1
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-10 10:54:13 -0400

    Create updateEpisodeInfo.java

---
+++ src/Information Processing/updateEpisodeInfo.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class updateEpisodeInfo {
+
+        public static ExecResult execute(String sql) {
+            return WolfPubDB.executeUpdate(sql);
+        }
+
+        public static void showDetails(String tableName){
+            String sql = String.format("SELECT * FROM " + tableName + ";");
+            WolfPubDB.executeQuery(sql);
+        }
+
+        public static ExecResult run(Scanner reader) {
+            System.out.println("+------------------------------------+");
+            System.out.println("|      Podcast Episode Details       |");
+            System.out.println("+------------------------------------+");
+            System.out.println("");
+
+            showDetails("PodcastEpisodes");
+
+            System.out.println("+------------------------------------+");
+            System.out.println("| Please Submit the Following Inputs |");
+            System.out.println("+------------------------------------+");
+            System.out.println("");
+
+            System.out.println("Podcast Episode ID: ");
+            int peID = reader.nextInt();
+            reader.nextLine();
+
+            System.out.println("Podcast ID: ");
+            int pID = reader.nextInt();
+            reader.nextLine();
+
+            System.out.println("Attribute you want to update: ");
+            String attributeName = reader.nextLine();
+
+            System.out.println("New attribute value: ");
+
+            String sql = "";
+
+            if(attributeName.equals("PETitle")){
+                String updatedAttributeValue = reader.nextLine();
+
+                sql = "UPDATE PodcastEpisodes SET %s='%s' WHERE PEID = (%d) AND PID = (%d);" + "\n" + "\n";
+                sql = String.format(sql, attributeName, updatedAttributeValue, peID, pID);
+
+            } else if(attributeName.equals("PEDuration") || attributeName.equals("PEReleaseDate")){
+                String updatedAttributeValue = reader.nextLine();
+
+                sql = "UPDATE PodcastEpisodes SET %s='%s' WHERE PEID = (%d) AND PID = (%d);" + "\n" + "\n";
+                sql = String.format(sql, attributeName, updatedAttributeValue, peID, pID);
+            } else if(attributeName.equals("ListenerCount") || attributeName.equals("AdCount")){
+                int updatedAttributeValue = reader.nextInt();
+                reader.nextLine();
+
+                sql = "UPDATE PodcastEpisodes SET %s=%d WHERE PEID = (%d) AND PID = (%d);" + "\n" + "\n";
+                sql = String.format(sql, attributeName, updatedAttributeValue, peID, pID);
+            }
+
+            return execute(sql);
+ }
