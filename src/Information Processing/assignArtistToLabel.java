3682adb6708b16e6dd34affbd322ed1913c2afd1
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-10 16:08:57 -0400

    Create assignArtistToLabel.java

---
+++ src/Information Processing/assignArtistToLabel.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class assignArtistToLabel {
+
+    public static Result execute(String sql) {
+        return Connect.executeUpdate(sql);
+    }
+
+    public static void showDetails(String tableName){
+        String sql = String.format("SELECT * FROM " + tableName + ";");
+        Connect.executeQuery(sql);
+    }
+
+    public static ExecResult run(Scanner reader) {
+        System.out.println("+------------------------------------+");
+        System.out.println("|           Artist Details            |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        showDetails("Artists");
+
+        System.out.println("+------------------------------------+");
+        System.out.println("| Please Submit the Following Inputs |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        System.out.println("Artist ID: ");
+        int artistID = reader.nextInt();
+        reader.nextLine();
+
+         System.out.println("RecordLabel ID: ");
+        int rlID = reader.nextInt();
+        reader.nextLine();
+
+        String sql = "";
+        sql = "UPDATE Artists SET RLID=%d WHERE ArtistID=(%d);" + "\n" + "\n";
+        sql = String.format(sql, rlID, artistID);
+        }
+
+        return execute(sql);
+    }
+}
