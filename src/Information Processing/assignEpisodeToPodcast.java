0e4d6b4ad4fb15067598c9e4a74522148a97bfb0
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-10 16:21:57 -0400

    Create assignEpisodeToPodcast.java

---
+++ src/Information Processing/assignEpisodeToPodcast.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class assignEpisodeToPodcast {
+
+    public static Result execute(String sql) {
+        return Connect.executeUpdate(sql);
+    }
+
+    public static void showDetails(String tableName){
+        String sql = String.format("SELECT * FROM " + tableName + ";");
+        Connect.executeQuery(sql);
+    }
+
+    public static ExecResult run(Scanner reader) {
+        System.out.println("+------------------------------------+");
+        System.out.println("|           PodcastEpisode Details   |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        showDetails("PodcastEpisodes");
+
+        System.out.println("+------------------------------------+");
+        System.out.println("| Please Submit the Following Inputs |");
+        System.out.println("+------------------------------------+");
+        System.out.println("");
+
+        System.out.println("Podcast Episode ID: ");
+        int peID = reader.nextInt();
+        reader.nextLine();
+
+         System.out.println("Podcast ID: ");
+        int pID = reader.nextInt();
+        reader.nextLine();
+
+        String sql = "";
+        sql = "UPDATE Artists SET PID=%d WHERE PEID=(%d);" + "\n" + "\n";
+        sql = String.format(sql, pID, peID);
+        }
+
+        return execute(sql);
+    }
+}
