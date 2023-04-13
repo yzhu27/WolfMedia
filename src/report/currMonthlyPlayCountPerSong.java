146606e71f50ad5957527d4bbb4ce63897017704
Yang <yhzhu27@gmail.com>
2023-04-13 21:30:08 +0000

    add curr and prev for total payment

---
+++ src/report/currMonthlyPlayCountPerSong.java
@@ -0,0 +1,41 @@
+package report;
+import java.util.Scanner;
+import java.sql.*;
+import util.DBTablePrinter;
+import util.*;
+
+
+public class currMonthlyPlayCountPerSong {
+
+
+    public static String execute(int SongID) {
+
+		String sql = 
+		"SELECT Playcount, Title, SongID FROM " +
+		"Songs WHERE SongID=%d; ";
+        
+		sql = String.format(sql, SongID);
+        
+		return queryExecuter.execute(sql);
+	}
+
+    public static String run(Scanner reader) throws SQLException{
+        System.out.println("+------------------------------------+");
+		System.out.println("|             Song Details           |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+		DBTablePrinter.printTable("Songs");
+
+		System.out.println("+------------------------------------+");
+		System.out.println("| Please Submit the Following Inputs |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+		System.out.println("Song ID: ");
+		int SongID = reader.nextInt();
+		reader.nextLine();
+
+		return execute(SongID);	
+	}
+}
---
+++ src/report/prevMonthlyPlayCountPerAlbum.java
@@ -0,0 +1,42 @@
+package report;
+import java.util.Scanner;
+import java.sql.*;
+import util.DBTablePrinter;
+import util.*;
+
+
+public class prevMonthlyPlayCountPerAlbum {
+
+	public static String execute(int AlbumID) {
+		String sql = 
+		"SELECT * FROM " +
+		"(SELECT SUM(SRPlaycount) AS sum, Title, AlbumID, SRDate FROM  " +
+		"(SELECT s.* FROM Albums as al  " +
+		"JOIN Songs as s ON al.AlbumID=s.AlbumID) AS songalbum " +
+		"JOIN SongRecords as sr on sr.SongID=songalbum.SongID GROUP BY SRDate, AlbumID ) as summ " +
+		"WHERE AlbumID=%d; ";
+
+		sql = String.format(sql, AlbumID);
+		return queryExecuter.execute(sql);
+	}
+
+	public static String run(Scanner reader) throws SQLException{
+		System.out.println("+------------------------------------+");
+		System.out.println("|            Artist Details          |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+		DBTablePrinter.printTable("Albums");
+
+		System.out.println("+------------------------------------+");
+		System.out.println("| Please Submit the Following Inputs |");
+		System.out.println("+------------------------------------+");
+		System.out.println("");
+
+		System.out.println("Album ID: ");
+		int AlbumID = reader.nextInt();
+		reader.nextLine();
+
+		return execute(AlbumID);	
+	}
+}
