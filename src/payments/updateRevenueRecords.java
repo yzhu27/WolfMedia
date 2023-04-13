ab7be886635cb11cf551055deefe06b517486006
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-13 17:14:58 -0400

    Update updateRevenueRecords.java

--- src/payments/updateRevenueRecords.java
+++ src/payments/updateRevenueRecords.java
@@ -5,6 +5,10 @@ import java.sql.*;
 
 import java.util.Scanner;
 
+/**
+ *  This Class used for updating a revenue record in RevenueRecords table.
+ */
+
 public class updateRevenueRecords {
 
     public static String run(Scanner reader) throws SQLException{
