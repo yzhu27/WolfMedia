032526d25528e7b8a005912dca0ae38072236b9f
HARSHAVARDHAN BANDARU <114872378+hbandar@users.noreply.github.com>
2023-04-13 15:36:29 -0400

    Update updatePodcastInfo.java

--- src/process/updatePodcastInfo.java
+++ src/process/updatePodcastInfo.java
@@ -6,6 +6,10 @@ import java.sql.*;
 import util.*;
 import java.sql.*;
 
+/**
+ *  This Class used for executing the updatePodcastInfo API operation by updating the Podcasts Table.
+ */
+
 public class updatePodcastInfo {
 
       public static String execute(String sql) {
@@ -55,36 +59,7 @@ public class updatePodcastInfo {
           String newValue = reader.nextLine();
 
           return execute(podcastID, attribute, newValue);
-
-//          System.out.println("Attribute you want to update: ");
-//          String attributeName = reader.nextLine();
-//
-//          System.out.println("New attribute value: ");
-//
-//          String sql = "";
-//
-//          if(attributeName.equals("PName") || attributeName.equals("PLanguage") || attributeName.equals("PCountry")){
-//              String updatedAttributeValue = reader.nextLine();
-//
-//              sql = "UPDATE Podcasts SET %s='%s' WHERE PID = (%d);" + "\n" + "\n";
-//              sql = String.format(sql, attributeName, updatedAttributeValue, podcastID);
-//
-//          } else if(attributeName.equals("PRating")){
-//              float updatedAttributeValue = reader.nextFloat();
-//              reader.nextLine();
-//
-//              sql = "UPDATE Podcasts SET %s=%f WHERE PID = (%d);" + "\n" + "\n";
-//              sql = String.format(sql, attributeName, updatedAttributeValue, podcastID);
-//
-//          } else if(attributeName.equals("PSubscribers") || attributeName.equals("EpisodeCount") || attributeName.equals("PHID")){
-//              int updatedAttributeValue = reader.nextInt();
-//              reader.nextLine();
-//
-//              sql = "UPDATE Podcasts SET %s=%d WHERE PID = (%d);" + "\n" + "\n";
-//              sql = String.format(sql, attributeName, updatedAttributeValue, podcastID);
-//          }
-//
-//          return execute(sql);
+       
       }
     public static String execute(int ID, String attribute, String newValue) {
 
